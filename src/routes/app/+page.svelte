f2165b3005e9758b86e9dad8066ec0c9832b3ec9
nicolai.walsemann@gmx.com <nicolai.walsemann@gmx.com>
2025-10-01 18:57:48 +0200

    test

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -10,7 +10,7 @@
 		<h1 class="text-xl font-semibold">{m.app_welcome()}</h1>
 		<p class="text-sm text-gray-600">{m.app_signed_in_as({ email: data.user.email ?? '' })}</p>
 	</section>
-{:else}   
+{:else}
 	<section class="p-4">
 		<h1 class="text-xl font-semibold">{m.app_not_auth()}</h1>
 		<p class="text-sm text-gray-600">{m.app_redirect_login()}</p>
